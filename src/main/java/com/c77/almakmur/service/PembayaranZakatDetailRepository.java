c8f457a2f23ee2adf50927abff9ca3c5ca136953
Yudhi Yuswaldi <yuswaldi@gmail.com>
2014-12-25 11:37:26 +0700

    Repository Dao PembayaranZakatDetail

---
+++ src/main/java/com/c77/almakmur/service/PembayaranZakatDetailRepository.java
@@ -0,0 +1,8 @@
+package com.c77.almakmur.service;
+
+import com.c77.almakmur.domain.PembayaranZakatDetail;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PembayaranZakatDetailRepository extends PagingAndSortingRepository<PembayaranZakatDetail, String>{
+    
+}

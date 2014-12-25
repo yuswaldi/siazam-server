83b078a6dbd30c5bdb1a728e004fc5b9147933ad
Yudhi Yuswaldi <yuswaldi@gmail.com>
2014-12-25 11:36:57 +0700

    Repository Dao SatuanZakat

---
+++ src/main/java/com/c77/almakmur/service/SatuanZakatRepository.java
@@ -0,0 +1,8 @@
+package com.c77.almakmur.service;
+
+import com.c77.almakmur.domain.SatuanZakat;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SatuanZakatRepository extends CrudRepository<SatuanZakat, Long>{
+    
+}
